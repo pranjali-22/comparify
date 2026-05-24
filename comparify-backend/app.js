5d52d7ce341538bfd6e1dca674c02ed9b32adfa7
Pranjali <pranjalidas2201@gmail.com>
2026-05-24 06:03:03 -0700

    app

--- comparify-backend/app.js
+++ comparify-backend/app.js
@@ -1,13 +1,15 @@
 require("dotenv").config();
 const express = require("express");
-const connectDB = require("./database/db");
+const compareRoutes = require("./routes/compareRoutes");
 
 const app = express();
-connectDB();
-
 app.use(express.json());
+app.use("/compare", compareRoutes);
 
-app.get("/", (req, res) => res.send("Comparify backend running"));
+app.use((err, req, res, next) => {
+    console.error(err.message);
+    res.status(500).json({ error: "Something went wrong" });
+});
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
--- comparify-backend/routes/foodRoutes.js
+++
@@ -1,7 +0,0 @@
-const express = require("express");
-const { compareFood } = require("../controllers/foodController");
-const router = express.Router();
-
-router.post("/compare", compareFood);
-
-module.exports = router;
